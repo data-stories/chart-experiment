0fec37595aa791712a8c860d690aa88f6c671325
Aneta Swianiewicz <anette812s@gmail.com>
2021-02-21 16:28:05 +0000

    get_all_records function and variations

--- src/chart_params.py
+++ src/chart_params.py
@@ -1,5 +1,8 @@
 from itertools import product
 
+import numpy as np
+import pandas as pd
+
 from data_setup import PlottedData
 from base_params import BaseParams
 
@@ -13,9 +16,104 @@ class ChartParams(BaseParams):
         self.variations_pie = self._get_pie_dict()
         self.variations_donut = self._get_donut_dict()
         self.variations_bar = self._get_bar_dict()
-        
 
-    def get_variations(self, variations_dict):
+        self.pie_variations, self.donut_variations, self.bar_variations = self.get_all_variations()
+    
+    def get_all_records(self):
+        list_of_cols = [
+                'Type', 'Hue', 'Displayed_data', 'Wedge_explode', 'Source_annotation_location', 'Rotation_Orientation',
+                'Percent_annotation_precision',	'Annotation_distance',	'Font',	'Fontsize',	'Legend_location',
+                'Sample_annotation', 'Donuthole_size', 'Grid', 'Bars_width', 'Bar_Orientation', 'Bar_spacing',
+                'Error_bar-value_annotation'
+            ]
+        pie_data_ = [(
+                'PIE', 
+                item[0],
+                item[1],
+                self.variations_pie['wedge_explode'][item[2]],
+                self.variations_pie['source_annotation'][item[3]],
+                self.variations_pie['orientation'][item[4]],
+                self.variations_pie['percent_annotation'][item[5]],
+                self.variations_pie['annotation_distance'][item[6]],
+                self.variations_pie['font'][item[7]],
+                self.variations_pie['fontsize'][item[8]],
+                item[9],
+                self.variations_pie['sample_annot'][item[10]],
+                np.NaN,
+                np.NaN,
+                np.NaN,
+                np.NaN,
+                np.NaN,
+                np.NaN
+            ) for item in self.pie_variations]
+        
+        donut_data_ = [(
+                'DONUT', 
+                item[0],
+                item[1],
+                self.variations_donut['wedge_explode'][item[3]],
+                self.variations_donut['source_annotation'][item[4]],
+                self.variations_donut['orientation'][item[5]],
+                self.variations_donut['percent_annotation'][item[6]],
+                self.variations_donut['annotation_distance'][item[7]],
+                self.variations_donut['font'][item[8]],
+                self.variations_donut['fontsize'][item[9]],
+                item[10],
+                self.variations_donut['sample_annot'][item[11]],
+                self.variations_donut['donuthole_size'][item[2]],
+                np.NaN,
+                np.NaN,
+                np.NaN,
+                np.NaN,
+                np.NaN
+            ) for item in self.donut_variations]
+        
+        bar_data_ = [(
+            'BAR', 
+            item[0],
+            item[1],
+            np.NaN,
+            self.variations_bar['source_annotation'][item[4]],
+            np.NaN,
+            self.variations_bar['percent_annotation'][item[3]],
+            np.NaN,
+            self.variations_bar['font'][item[9]],
+            self.variations_bar['fontsize'][item[10]],
+            item[11],
+            self.variations_bar['sample_annot'][item[12]],
+            np.NaN,
+            self.variations_bar['grid'][item[2]],
+            self.variations_bar['bars_width'][item[5]],
+            self.variations_bar['orientation'][item[6]],
+            self.variations_bar['bar_spacing'][item[7]],
+            self.variations_bar['errorbar_value-loc'][item[8]]
+        ) for item in self.bar_variations]
+
+        dfs_ = [pie_data_,donut_data_,bar_data_]
+
+        complete_df = pd.concat([pd.DataFrame(i, columns=list_of_cols) for i in dfs_],
+          ignore_index=True)
+
+        return complete_df
+    
+    def get_all_variations(self):
+        pie_variations = self._get_variations(self.variations_pie)
+        donut_variations = self._get_variations(self.variations_donut)
+        bar_variations_raw = self._get_variations(self.variations_bar)
+
+        # getting rid of legend variations for dd02 & dd12 in bar_variations
+        vs = []
+        for i in bar_variations_raw:
+            if i[1] in ['dd02','dd12']:
+                _i = list(i)
+                _i[11] = 'llno'
+                i = tuple(_i)
+            vs.append(i)
+        bar_variations = list(set(vs))
+
+        return pie_variations, donut_variations, bar_variations
+
+    def _get_variations(self, variations_dict):
         combinations = product(*(variations_dict[Name] for Name in variations_dict))
         return list(combinations)
     
