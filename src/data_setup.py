767ed56731ab82d49efdeb526ff8bdaf345109e6
Aneta Swianiewicz <anette812s@gmail.com>
2021-03-05 12:19:51 +0000

    new data category dd09

--- src/data_setup.py
+++ src/data_setup.py
@@ -11,11 +11,13 @@ class PlottedData:
                 "data_donut_pie": {
                         "dd02": self._get_dd02_PP_PD(),
                         "dd06": self._get_dd06_PP_PD(),
+                        #"dd09": self._get_dd09_PP_PD(),
                         "dd12": self._get_dd12_PP_PD()
                     },
                 "data_bar": {
                         "dd02": self._get_dd02_PB(),
                         "dd06": self._get_dd06_PB(),
+                        #"dd09": self._get_dd09_PB(),
                         "dd12": self._get_dd12_PB()
                     }
                 }
@@ -163,5 +165,67 @@ class PlottedData:
             "annot": PB_12_annot
         }
         return dd12
+
+    def _get_dd09_PP_PD(self):
+        data_2018 = self.DATA[self.DATA["year"] == 2018]
+        PP_PD_09_vals = [
+                len(data_2018[(data_2018["temp"] > 0) & (data_2018["temp"] <=15)]),
+                len(data_2018[(data_2018["temp"] > 15) & (data_2018["temp"] <=30)]),
+                len(data_2018[(data_2018["temp"] > 30) & (data_2018["temp"] <=45)]),
+                len(data_2018[(data_2018["temp"] > 45) & (data_2018["temp"] <= 60)]),
+                len(data_2018[(data_2018["temp"] > 60) & (data_2018["temp"] <= 75)]),
+                len(data_2018[(data_2018["temp"] > 75) & (data_2018["temp"] <= 90)]),
+                len(data_2018[(data_2018["temp"] > 90) & (data_2018["temp"] <= 105)]),
+                len(data_2018[(data_2018["temp"] > 105) & (data_2018["temp"] <= 120)]),
+                len(data_2018[data_2018["temp"] > 120])
+        ]
+
+        PP_PD_09_labels = [
+                        '0-15 mm',
+                        '15-30 mm',
+                        '30-45 mm',
+                        '45-60 mm',
+                        '60-75 mm',
+                        '75-90 mm',
+                        '90-105 mm',
+                        '105-120 mm',
+                        'over 120 mm'           
+        ]
+
+        PP_PD_09_title = "Number of Days by Rainfall"
+
+        dd09 = {
+            "vals": PP_PD_09_vals,
+            "labels": PP_PD_09_labels,
+            "title": PP_PD_09_title
+        }
+        return dd09
+
+    def _get_dd09_PB(self):
+        d = self.DATA.groupby(["year", "month", "clouds"]).count().iloc[:,:1].rename(columns={"temp":"count"})
+        means = {}
+        for j in range(1,13):
+            means[j] = [np.mean(d.xs(j, level="month" ).to_numpy().reshape((3,3))[:,i]) for i in range(3)]
+
+        errs = {}
+        for j in range(1,13):
+            errs[j] = [np.std(d.xs(j, level="month" ).to_numpy().reshape((3,3))[:,i]) for i in range(3)]
+
+        # three arbitrarily chosen months
+        PB_9_vals = [means[3], means[6], means[12]] 
+        PB_9_error = [errs[3], errs[6], errs[12]]
+
+        PB_9_labels = ["March","June", "December"]
+        PB_9_xticks = ["Clear", "Partly Cloudy", "Very Cloudy"]
+        PB_9_title = "Average Cloundiness by Month"
+        
+        dd09 = {
+            "vals": PB_9_vals,
+            "errors": PB_9_error,
+            "xticks": PB_9_xticks,
+            "labels": PB_9_labels,
+            "title": PB_9_title
+        } 
+        return dd09
 
         
