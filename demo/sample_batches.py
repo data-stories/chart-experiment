d0d386ea25192f455913d69d2e006a39e0b86639
Aneta Swianiewicz <anette812s@gmail.com>
2021-04-11 22:21:15 +0100

    demo notebook

---
+++ demo/sample_batches.py
@@ -0,0 +1,68 @@
+sample_batches ={
+    "0": {
+    "pie": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llcl", "pano", "we00", "ro270","ad95"],
+        ["CR", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95"]
+    ],
+
+    "donut": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llcl", "pano", "we00", "ro270","ad95","dh2"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07","dh5"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11","dh2"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95", "dh8"]
+    ],
+
+    "bar": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llul", "pano", "e00a0", "bw6", "bs000", "boh", "gr0y"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llur", "pa05", "e00a1", "bw4", "bs000", "bov", "gr0y"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llul", "pano", "e10a1", "bw6", "bs000", "boh", "gr00"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llur", "pa03", "e11a1", "bw8", "bs000", "bov", "gr00"]
+    ]
+},
+    "1": {
+    "pie": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llcl", "pano", "we00", "ro270","ad95"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95"]
+    ],
+
+    "donut": [
+        ["MR", "dd06", "sano", "sm0", "ftfa", "fs16", "llcl", "pano", "we00", "ro270","ad95","dh2"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07","dh5"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11","dh2"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95", "dh8"]
+    ],
+
+    "bar": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llul", "pano", "e00a0", "bw6", "bs000", "boh", "gr0y"],
+        ["CR", "dd06", "sa00", "sm1", "ftse", "fs18", "llur", "pa05", "e00a1", "bw4", "bs000", "bov", "gr0y"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llul", "pano", "e10a1", "bw6", "bs000", "boh", "gr00"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llur", "pa03", "e11a1", "bw8", "bs000", "bov", "gr00"]
+    ]
+},
+    "2": {
+    "pie": [
+        ["MR", "dd06", "sano", "sm0", "ftns", "fs16", "llcl", "pano", "we00", "ro270","ad95"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95"]
+    ],
+
+    "donut": [
+        ["MR", "dd06", "sano", "sm0", "ftfa", "fs16", "llcl", "pano", "we00", "ro270","ad95","dh2"],
+        ["CR", "dd06", "sa00", "sm1", "ftse", "fs18", "llcr", "pa05", "we05", "ro000", "ad07","dh5"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llcl", "pano", "we05", "ro090", "ad11","dh2"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llcr", "pa03", "we10", "ro000", "ad95", "dh8"]
+    ],
+
+    "bar": [
+        ["MG", "dd06", "sano", "sm0", "ftfa", "fs16", "llul", "pano", "e00a0", "bw6", "bs000", "boh", "gr0y"],
+        ["CL", "dd06", "sa00", "sm1", "ftse", "fs18", "llur", "pa05", "e00a1", "bw4", "bs000", "bov", "gr0y"],
+        ["CM", "dd12", "sano", "sm0", "ftns", "fs12", "llul", "pano", "e10a1", "bw6", "bs000", "boh", "gr00"],
+        ["MB", "dd12", "sa00", "sm1", "ftns", "fs14", "llur", "pa03", "e11a1", "bw8", "bs000", "bov", "gr00"]
+    ]
+}
+}
